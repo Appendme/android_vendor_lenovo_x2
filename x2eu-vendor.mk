cccbed227035112e242d0380df804021f9984697
Appendme <appendme@yandex.ru>
2016-09-29 17:14:06 +0700

    push

---
+++ x2eu-vendor.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/x2eu/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/x2eu/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/x2eu/etc,system/etc)
+
+# PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/x2eu/app,system/app)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/x2eu/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/x2eu/vendor,system/vendor)
